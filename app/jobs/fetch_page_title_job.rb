a41b02ad0cde6845c0be0188456dcb845f94e80d
Chidi Ekuma <bukki.arise@yahoo.co.uk>
2020-12-20 19:18:09 +0100

    Adds implementation for `FetchPageTitleJob`

--- app/jobs/fetch_page_title_job.rb
+++ app/jobs/fetch_page_title_job.rb
@@ -1,3 +1,16 @@
 class FetchPageTitleJob < ApplicationJob
+  require 'open-uri'
+  require 'nokogiri'
 
+  queue_as :default
+
+  def perform(short_url)
+    html = open(short_url.original_url)
+    doc = Nokogiri::HTML(html)
+    page_title = doc.css('title').text
+    short_url.update_attribute(:title, page_title) if page_title.present?
+  rescue SocketError => e
+    logger = Rails.logger
+    logger.error e
+  end
 end
